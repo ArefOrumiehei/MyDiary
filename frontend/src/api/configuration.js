ba493ba131910a504d1266f7b8e3b7b364accd24
ArefOrumiehei <areforumiehei@gmail.com>
2025-07-04 14:53:19 +0330

    Remove logs

--- frontend/src/api/configuration.js
+++ frontend/src/api/configuration.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-console.log("[API] base =", import.meta.env.VITE_API_BASE_URL);
-
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: false,
